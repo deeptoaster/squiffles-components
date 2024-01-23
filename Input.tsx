05919578e10a55515fc5213130ba39c342eb8280
Deep Toaster <deeptoaster@gmail.com>
2024-01-23 04:54:23 -0500

    support select inputs

--- Input.tsx
+++ Input.tsx
@@ -3,33 +3,66 @@ import { ChangeEvent, RefObject, useMemo } from 'react';
 
 import './Input.css';
 
-export default function Input(props: {
+type CommonInputProps<Value extends number | string> = {
   id: string;
-  inputRef: RefObject<HTMLInputElement>;
   label: string;
-  onUpdate: (value: string) => void;
-  value: string;
-}): JSX.Element {
-  const { id, inputRef, label, onUpdate, value } = props;
+  onUpdate: (value: Value) => void;
+  value: Value;
+};
+
+type Option<Value extends number | string> = {
+  label: string;
+  value: Value;
+};
+
+type SelectInputProps<Value extends number | string> = {
+  inputRef?: RefObject<HTMLSelectElement>;
+  options: ReadonlyArray<Option<Value>>;
+};
+
+type TextInputProps = {
+  inputRef?: RefObject<HTMLInputElement>;
+};
+
+export default function Input<Value extends number | string>(
+  props: CommonInputProps<Value> & (SelectInputProps<Value> | TextInputProps)
+): JSX.Element {
+  // eslint-disable-next-line react/prop-types
+  const { id, inputRef, label, onUpdate, options, value } =
+    props as CommonInputProps<Value> &
+      Partial<SelectInputProps<Value> & TextInputProps>;
 
   const handleChange = useMemo(
     () =>
-      (event: ChangeEvent<HTMLInputElement>): void =>
-        onUpdate(event.currentTarget.value),
-    [onUpdate],
+      (event: ChangeEvent<HTMLInputElement | HTMLSelectElement>): void =>
+        onUpdate(event.currentTarget.value as Value),
+    [onUpdate]
   );
 
   return (
     <div className="form-control">
       <label htmlFor={id}>{label}</label>
       <div className="input-group">
-        <input
-          id={id}
-          onChange={handleChange}
-          ref={inputRef}
-          type="text"
-          value={value}
-        />
+        {options != null ? (
+          <select id={id} onChange={handleChange} ref={inputRef} value={value}>
+            {/* eslint-disable-next-line react/prop-types */}
+            {options.map(
+              ({ label, value }: Option<Value>): JSX.Element => (
+                <option key={value} value={value}>
+                  {label}
+                </option>
+              )
+            )}
+          </select>
+        ) : (
+          <input
+            id={id}
+            onChange={handleChange}
+            ref={inputRef}
+            type="text"
+            value={value}
+          />
+        )}
       </div>
     </div>
   );

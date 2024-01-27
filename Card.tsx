1279f1e39701f19647263c44774f3d9d094e9195
Deep Toaster <deeptoaster@gmail.com>
2024-01-27 06:09:38 -0500

    add 40em cards

--- Card.tsx
+++ Card.tsx
@@ -6,7 +6,7 @@ import './Card.css';
 export default function Card(props: {
   children: React.ReactNode;
   padded?: boolean;
-  width?: 20 | 30 | null;
+  width?: 20 | 30 | 40 | null;
 }): JSX.Element {
   const { children, padded = true, width = null } = props;
 
@@ -15,7 +15,8 @@ export default function Card(props: {
       className={classNames({
         'card-no-padding': !padded,
         'card-width-20': width === 20,
-        'card-width-30': width === 30
+        'card-width-30': width === 30,
+        'card-width-40': width === 40
       })}
     >
       {children}
